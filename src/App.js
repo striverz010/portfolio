c2dc0fa978ec0890bfded1361f038a270d51c562
striverz010 <striveredu010@gmail.com>
2024-03-27 08:23:13 +0530

    created until the about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./components/Navbar/navbar";
 import Intro from "./components/Intro/intro";
+import About from "./components/About/about";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>  
       <Intro/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/About/about.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ab1 from "../../assets/ab1.png";
+import ab2 from "../../assets/ab2.png"
+import ab3 from "../../assets/ab3.png"
+import "./about.css"
+const About=()=>{
+    return(
+        <section className="about">
+
+            <h1 align="center" className="about-tag">I Know That Good Dev <br/>Means Good Business</h1>
+
+            <div className="about-bars">
+
+                <div className="about-bar">
+                    <img src={ab1} alt="about-img" className="ab-img"></img>
+                    <h2>Problem-Solving</h2>
+                    <p>I am a good problem solver and love to solve real world problems.</p>
+                </div>
+
+                <div className="about-bar">
+                    <img src={ab2}  alt="about-img" className="ab-img"></img>
+                    <h2>Developer</h2>
+                    <p>I am a developer with passion for developing neat and functional projects.</p>
+                </div>
+
+                <div className="about-bar">
+                    <img src={ab3}  alt="about-img" className="ab-img"></img>
+                    <h2>Coder</h2>
+                    <p>I am enthusiastic coder and love to apply my skills in development.</p>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default About;
